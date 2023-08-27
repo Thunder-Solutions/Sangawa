5c9f38a7330d5817051307afdf123d0d9ac2485b
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-08-27 01:40:26 -0400

    consolidating menu components where possible

--- src/components/menuBar.tsx
+++ src/components/menuBar.tsx
@@ -3,11 +3,58 @@ import Link from 'next/link';
 import { useContext, useState } from 'react';
 import { useCSS, Scope } from 'react-shadow-scope';
 import { GlobalContent } from './page';
-import MobileNav from './mobileNav';
-import HamburgerIcon from './hamburgerIcon';
 import { getTypedContent } from '@/utilities/utilities';
 import Icon from './icon';
 import { theme } from '@/utilities/theme';
+import PageNav from './pageNav';
+
+type MenuTogglerProps = {
+	isOpen: boolean;
+	onClick: () => void;
+};
+
+const MenuToggler = ({ isOpen, onClick }: MenuTogglerProps) => {
+	const css = useCSS();
+	const stylesheet = css`
+		button {
+			background: none;
+			border: none;
+			height: 2rem;
+			display: grid;
+			place-items: center;
+			z-index: 10;
+			cursor: pointer;
+			margin: 0 0 0 1rem;
+			padding: 0;
+		}
+		.line {
+			width: 2rem;
+			height: 0.25rem;
+			border-radius: 0.625rem;
+			background-color: white;
+			transform-origin: 0.1rem;
+			transition: all 0.3s linear;
+		}
+		.line-1 {
+			transform: ${isOpen ? 'rotate(45deg)' : 'rotate(0)'};
+		}
+		.line-2 {
+			opacity: ${isOpen ? '0' : '1'};
+		}
+		.line-3 {
+			transform: ${isOpen ? 'rotate(-45deg)' : 'rotate(0)'};
+		}
+	`;
+	return (
+		<Scope stylesheets={[theme, stylesheet]}>
+			<button onClick={onClick}>
+				<div className="line line-1" />
+				<div className="line line-2" />
+				<div className="line line-3" />
+			</button>
+		</Scope>
+	);
+};
 
 type LinkContent = { text: string; href: string };
 type TextContent = { text: string };
@@ -25,96 +72,31 @@ const MenuBar = () => {
 		.logo {
 			height: 4rem;
 		}
-		nav {
+		.menu-items {
 			display: flex;
 			gap: 0.5rem;
 			align-items: center;
-		}
-		.nav-link {
-			color: var(--color-brand-1-c);
-			text-decoration: none;
-			font-size: 1.2rem;
-		}
-		.nav-link:hover {
-			background-color: var(--color-nav-hover);
-		}
-		nav > .nav-link {
-			height: 100%;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			padding: 0 0.75rem;
-		}
-		.subnav {
-			position: relative;
-			height: calc(100% + 0.75rem);
-			cursor: pointer;
-		}
-		.subnav .subnav-btn {
-			border: none;
-			outline: none;
-			background-color: transparent;
-			font-family: inherit;
-			margin: calc(20% + 1rem) 0 0 0;
-			cursor: pointer;
-		}
-		.subnav:hover {
-			background-color: var(--color-nav-hover);
-		}
-		.subnav:hover .subnav-content {
-			display: flex;
-		}
-		.subnav-content {
-			display: none;
-			flex-direction: column;
-			gap: 0.375rem;
-			position: absolute;
-			top: 4.375rem;
-			right: 0;
-			background-color: var(--color-brand-1);
-			z-index: 1;
-			border-radius: 0.375rem;
-			text-align: right;
-			min-width: 11rem;
-			padding: 0.75rem 0;
-		}
-		.subnav-link {
-			padding: 0.25rem 0.625rem;
-		}
-		.register-link {
-			font-family: 'Brasspounder';
-			background-color: var(--color-brand-1-c);
-			border: 0 solid;
-			border-radius: 0.5rem;
-			color: var(--color-brand-1);
-			text-decoration: none;
-			padding: 1rem 2rem;
-			display: inline-block;
 		}
 		.icon {
 			color: var(--color-brand-1-c);
 			font-size: 2rem;
 		}
-		.mobile-nav-icon {
+		.desktop-nav {
 			display: none;
 		}
-		@media screen and (max-width: 767px) {
-			nav.desktop-nav .nav-link,
-			a.register-link {
+		@media screen and (min-width: 70em) {
+			.mobile-nav {
 				display: none;
 			}
-			nav .mobile-nav-icon {
+			.desktop-nav {
 				display: block;
 			}
 		}
 	`;
 	const global = useContext(GlobalContent);
-	const nav = getTypedContent<LinkContent>(global.nav);
-	const register = getTypedContent<LinkContent>(global.register)[0];
 	const title = getTypedContent<TextContent>(global.title)[0];
 	const facebook = getTypedContent<LinkContent>(global.facebook)[0];
 	const twitter = getTypedContent<LinkContent>(global.twitter)[0];
-
 	const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
 	const toggleMobileNav = () => {
@@ -127,46 +109,23 @@ const MenuBar = () => {
 				<Link href="/">
 					<Image className="logo" src="/sangawa-logo-light.svg" alt={title.content.text} height={50} width={165} />
 				</Link>
-				<nav className="desktop-nav">
-					{nav.map(({ content, children }) => {
-						if (Array.isArray(children) && children.length > 0) {
-							return (
-								<div key={content.href} className="subnav">
-									<button className="nav-link subnav-btn">{content.text}</button>
-									<div className="subnav-content">
-										{getTypedContent<LinkContent>(children).map(({ content }) => (
-											<Link key={content.href} href={content.href} className="nav-link subnav-link">
-												{content.text}
-											</Link>
-										))}
-									</div>
-								</div>
-							);
-						}
-
-						return (
-							<Link key={content.href} href={content.href} className="nav-link">
-								{content.text}
-							</Link>
-						);
-					})}
-
-					<Link href={register.content.href} className="register-link">
-						{register.content.text}
-					</Link>
+				<div className="menu-items">
+					<div className="desktop-nav">
+						<PageNav />
+					</div>
 
 					<Link href={twitter.content.href}>
-						<Icon type="twitter" title={twitter.content.text} className="icon icon--twitter" />
+						<Icon type="twitter" title={twitter.content.text} className="icon" />
 					</Link>
 					<Link href={facebook.content.href}>
-						<Icon type="facebook" title={facebook.content.text} className="icon icon--facebook" />
+						<Icon type="facebook" title={facebook.content.text} className="icon" />
 					</Link>
 
-					<div className="mobile-nav-icon">
-						<HamburgerIcon isOpen={mobileNavOpen} onClick={toggleMobileNav} />
-						<MobileNav isOpen={mobileNavOpen} />
+					<div className="mobile-nav">
+						<MenuToggler isOpen={mobileNavOpen} onClick={toggleMobileNav} />
+						<PageNav mobile={true} isOpen={mobileNavOpen} />
 					</div>
-				</nav>
+				</div>
 			</header>
 		</Scope>
 	);
---
+++ src/components/pageNav.tsx
@@ -0,0 +1,198 @@
+import { getTypedContent } from '@/utilities/utilities';
+import Link from 'next/link';
+import { useContext } from 'react';
+import { GlobalContent } from './page';
+import { Scope, useCSS } from 'react-shadow-scope';
+import { theme } from '@/utilities/theme';
+
+type LinkContent = { text: string; href: string };
+export type PageNavProps = {
+	isOpen?: boolean;
+	mobile?: boolean;
+};
+
+const PageNav = ({ isOpen = true, mobile = false }: PageNavProps) => {
+	const css = useCSS();
+	const mobileStylesheet = css`
+		.nav {
+			position: absolute;
+			top: 1.6rem;
+			right: 0;
+			display: ${isOpen ? 'flex' : 'none'};
+			flex-direction: column;
+			gap: 0.5rem;
+			align-items: flex-end;
+			background-color: var(--color-brand-1);
+			padding: 0.75rem 0;
+		}
+		.wrapper {
+			position: relative;
+		}
+		.nav-link {
+			color: var(--color-brand-1-c);
+			text-decoration: none;
+			width: 100%;
+			text-align: right;
+			padding: 0.25rem 0.75rem;
+			box-sizing: border-box;
+		}
+		.nav-link:focus-within {
+			background-color: var(--color-nav-hover);
+		}
+		.subnav {
+			position: relative;
+			cursor: pointer;
+			width: 100%;
+		}
+		.subnav .subnav-btn {
+			border: none;
+			outline: none;
+			background-color: transparent;
+			font-family: inherit;
+			cursor: pointer;
+			margin-left: -0.125rem;
+		}
+		.subnav:focus-within {
+			background-color: var(--color-nav-hover);
+		}
+		.subnav:focus-within .subnav-content {
+			display: flex;
+		}
+		.subnav-content {
+			display: none;
+			flex-direction: column;
+			gap: 0.375rem;
+			position: absolute;
+			top: -0.813rem;
+			right: 8.625rem;
+			background-color: var(--color-brand-1);
+			z-index: 1;
+			border-radius: 0.375rem;
+			text-align: right;
+			min-width: 11rem;
+			padding: 0.75rem 0;
+		}
+		.subnav-link {
+			padding: 0.25rem 0.625rem;
+		}
+		.register-link {
+			font-family: 'Brasspounder';
+			background-color: var(--color-brand-1-c);
+			border: 0 solid;
+			border-radius: 0.375rem;
+			color: var(--color-brand-1);
+			text-decoration: none;
+			padding: 0.625rem 1.25rem;
+			margin: 0.375rem 0.625rem;
+			display: inline-block;
+		}
+	`;
+	const desktopStylesheet = css`
+		.nav {
+			display: flex;
+			gap: 0.5rem;
+			height: 3rem;
+		}
+		.nav-link {
+			color: var(--color-brand-1-c);
+			text-decoration: none;
+			font-size: 1.2rem;
+		}
+		.nav-link:hover {
+			background-color: var(--color-nav-hover);
+		}
+		.nav > .nav-link {
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			padding: 0 0.75rem;
+		}
+		.subnav {
+			display: flex;
+			position: relative;
+			height: 100%;
+			cursor: pointer;
+		}
+		.subnav .subnav-btn {
+			border: none;
+			outline: none;
+			background-color: transparent;
+			font-family: inherit;
+			cursor: pointer;
+		}
+		.subnav:hover {
+			background-color: var(--color-nav-hover);
+		}
+		.subnav:hover .subnav-content {
+			display: flex;
+		}
+		.subnav-content {
+			display: none;
+			flex-direction: column;
+			gap: 0.375rem;
+			position: absolute;
+			top: 3rem;
+			right: 0;
+			background-color: var(--color-brand-1);
+			z-index: 1;
+			border-radius: 0.375rem;
+			text-align: right;
+			min-width: 11rem;
+			padding: 0.75rem 0;
+		}
+		.subnav-link {
+			padding: 0.25rem 0.625rem;
+		}
+		.register-link {
+			font-family: 'Brasspounder';
+			background-color: var(--color-brand-1-c);
+			border: 0 solid;
+			border-radius: 0.5rem;
+			color: var(--color-brand-1);
+			text-decoration: none;
+			padding: 1rem 2rem;
+			display: inline-block;
+		}
+	`;
+	const stylesheet = mobile ? mobileStylesheet : desktopStylesheet;
+	const global = useContext(GlobalContent);
+	const nav = getTypedContent<LinkContent>(global.nav);
+	const register = getTypedContent<LinkContent>(global.register)[0];
+	return (
+		<Scope stylesheets={[theme, stylesheet]}>
+			<div className="wrapper">
+				<nav className="nav">
+					{nav.map(({ content, children }) => {
+						if (Array.isArray(children) && children.length > 0) {
+							return (
+								<div key={content.href} className="subnav">
+									<button className="nav-link subnav-btn">{content.text}</button>
+									<nav className="subnav-content">
+										{getTypedContent<LinkContent>(children).map(({ content }) => (
+											<Link key={content.href} href={content.href} className="nav-link subnav-link">
+												{content.text}
+											</Link>
+										))}
+									</nav>
+								</div>
+							);
+						}
+
+						return (
+							<Link key={content.href} href={content.href} className="nav-link">
+								{content.text}
+							</Link>
+						);
+					})}
+
+					<Link href={register.content.href} className="register-link">
+						{register.content.text}
+					</Link>
+				</nav>
+			</div>
+		</Scope>
+	);
+};
+
+export default PageNav;
