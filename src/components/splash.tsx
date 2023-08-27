a2f86fb5b21545aaab8f1f4cc3baa96060c4e184
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-08-26 21:57:44 -0400

    adding new assets

--- src/components/splash.tsx
+++ src/components/splash.tsx
@@ -20,24 +20,26 @@ const stylesheet = css`
 		justify-content: center;
 	}
 	.logo {
-		width: 100%;
-		height: 15rem;
-		padding: 1rem;
-		object-fit: contain;
 		box-sizing: border-box;
+		height: 20.5rem;
+		object-fit: contain;
+		padding: 2rem 3rem;
+		max-width: 100%;
+		width: 60rem;
 	}
 	.scroll {
 		position: absolute;
 		bottom: 0;
 		width: 100%;
-		display: grid;
+		display: flex;
 		justify-content: center;
-		justify-items: center;
-		gap: 0.2rem;
-		padding-bottom: 2rem;
+		align-items: center;
+		gap: 0.5rem;
+		padding: 3rem 0 0.5rem;
 		color: var(--color-brand-1);
 		font-weight: bold;
 		font-size: 2rem;
+		background-image: linear-gradient(transparent, rgba(255, 255, 255, 0.6) 50%);
 	}
 `;
 
@@ -45,14 +47,14 @@ const Splash = () => {
 	return (
 		<div style={{ height: '100vh' }}>
 			<ParallaxBanner style={{ aspectRatio: '2 / 1', height: '100%' }}>
-				<ParallaxBannerLayer image="Tekko-Japanese-Anime-Convention-Dealers-Room.jpg" speed={20} opacity={[2, 0]} />
-				<ParallaxBannerLayer image="ice-texture.png" speed={-20} />
+				<ParallaxBannerLayer image="mount-fuji.webp" speed={30} opacity={[1, 0]} />
+				<ParallaxBannerLayer image="snowflakes-small.webp" speed={-10} opacity={[2, 0]} />
 				<ParallaxBannerLayer speed={0} opacity={[2, 0]}>
 					<Scope stylesheet={stylesheet}>
 						<div className="splash-container">
 							<div className="splash">
 								<h1>
-									<Image className="logo" src="sangawa-logo.svg" height={300} width={600} alt="Sangawa Project" />
+									<Image className="logo" src="sangawa-logo.svg" width={704} height={236} alt="Sangawa Project" />
 								</h1>
 							</div>
 							<div className="scroll">
@@ -62,6 +64,7 @@ const Splash = () => {
 						</div>
 					</Scope>
 				</ParallaxBannerLayer>
+				<ParallaxBannerLayer image="snowflakes-large.webp" speed={-30} />
 			</ParallaxBanner>
 		</div>
 	);
