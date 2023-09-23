567cf48560ea7781c7f052bb52c20f71a8f0a113
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-09-23 13:43:16 -0400

    refactoring and improving a11y

--- src/components/pageNav.tsx
+++ src/components/pageNav.tsx
@@ -1,8 +1,8 @@
 import { getTypedContent } from '@/utilities/utilities';
 import Link from 'next/link';
-import { HTMLAttributes, useContext } from 'react';
+import { HTMLAttributes, useContext, useState } from 'react';
 import { GlobalContext } from '@/api-client/context';
-import { Scope, useCSS } from 'react-shadow-scope';
+import { Scope, css } from 'react-shadow-scope';
 import { theme } from '@/utilities/theme';
 import Icon from './icon';
 
@@ -14,163 +14,162 @@ export type PageNavProps = {
 	mobile?: boolean;
 } & HTMLAttributes<HTMLDivElement>;
 
-const mobileKey = Symbol();
-const desktopKey = Symbol();
+const mobileStylesheet = css`
+	.nav {
+		position: absolute;
+		top: 1.6rem;
+		right: 0;
+		display: none;
+		flex-direction: column;
+		gap: 0.5rem;
+		align-items: flex-end;
+		background-color: var(--color-brand-1);
+		padding: 0.75rem 0;
+	}
+	.nav[aria-hidden='false'] {
+		display: flex;
+	}
+	.wrapper {
+		position: relative;
+	}
+	.nav-link {
+		color: var(--color-brand-1-c);
+		text-decoration: none;
+		width: 100%;
+		text-align: right;
+		padding: 0.25rem 0.75rem;
+		box-sizing: border-box;
+	}
+	.nav-link:focus-within {
+		background-color: var(--color-nav-hover);
+	}
+	.subnav {
+		position: relative;
+		cursor: pointer;
+		width: 100%;
+	}
+	.subnav .subnav-btn {
+		display: flex;
+		place-items: center;
+		justify-content: end;
+		border: none;
+		outline: none;
+		background-color: transparent;
+		font-family: inherit;
+		cursor: pointer;
+		margin-left: -0.125rem;
+	}
+	.subnav:focus-within {
+		background-color: var(--color-nav-hover);
+	}
+	.subnav-content[aria-hidden='false'] {
+		display: flex;
+	}
+	.subnav-content {
+		display: none;
+		flex-direction: column;
+		gap: 0.375rem;
+		position: absolute;
+		top: -0.813rem;
+		right: 8.625rem;
+		background-color: var(--color-brand-1);
+		z-index: 1;
+		border-radius: 0.375rem;
+		text-align: right;
+		min-width: 11rem;
+		padding: 0.75rem 0;
+	}
+	.subnav-link {
+		padding: 0.25rem 0.625rem;
+	}
+	.register-link {
+		font-family: 'Brasspounder';
+		background-color: var(--color-brand-1-c);
+		border: 0 solid;
+		border-radius: 0.375rem;
+		color: var(--color-brand-1);
+		text-decoration: none;
+		padding: 0.625rem 1.25rem;
+		margin: 0.375rem 0.625rem;
+		display: inline-block;
+	}
+`;
+const desktopStylesheet = css`
+	.nav {
+		display: flex;
+		gap: 0.5rem;
+		height: 3rem;
+	}
+	.nav-link {
+		color: var(--color-brand-1-c);
+		display: flex;
+		place-items: center;
+		padding: 0 1rem;
+		height: 100%;
+		text-decoration: none;
+		font-size: 1.2rem;
+	}
+	.nav-link:hover {
+		background-color: var(--color-nav-hover);
+	}
+	.nav > .nav-link {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		padding: 0 0.75rem;
+	}
+	.subnav {
+		display: flex;
+		position: relative;
+		height: 100%;
+		cursor: pointer;
+	}
+	.subnav .subnav-btn {
+		display: flex;
+		place-items: center;
+		border: none;
+		outline: none;
+		background-color: transparent;
+		font-family: inherit;
+		cursor: pointer;
+	}
+	.subnav:hover {
+		background-color: var(--color-nav-hover);
+	}
+	.subnav-content[aria-hidden='false'] {
+		display: flex;
+	}
+	.subnav-content {
+		display: none;
+		flex-direction: column;
+		gap: 0.375rem;
+		position: absolute;
+		top: 3rem;
+		right: 0;
+		background-color: var(--color-brand-1);
+		z-index: 1;
+		border-radius: 0.375rem;
+		text-align: right;
+		min-width: 15rem;
+		padding: 0.75rem 0;
+	}
+	.subnav-link {
+		padding: 0.25rem 0.625rem;
+	}
+	.register-link {
+		font-family: 'Brasspounder';
+		background-color: var(--color-brand-1-c);
+		border: 0 solid;
+		border-radius: 0.5rem;
+		color: var(--color-brand-1);
+		text-decoration: none;
+		padding: 1rem 2rem;
+		display: inline-block;
+	}
+`;
 
 const PageNav = ({ isOpen = true, mobile = false, ...forwardedProps }: PageNavProps) => {
-	const mobileCSS = useCSS(mobileKey);
-	const desktopCSS = useCSS(desktopKey);
-	const mobileStylesheet = mobileCSS`
-		.nav {
-			position: absolute;
-			top: 1.6rem;
-			right: 0;
-			display: ${isOpen ? 'flex' : 'none'};
-			flex-direction: column;
-			gap: 0.5rem;
-			align-items: flex-end;
-			background-color: var(--color-brand-1);
-			padding: 0.75rem 0;
-		}
-		.wrapper {
-			position: relative;
-		}
-		.nav-link {
-			color: var(--color-brand-1-c);
-			text-decoration: none;
-			width: 100%;
-			text-align: right;
-			padding: 0.25rem 0.75rem;
-			box-sizing: border-box;
-		}
-		.nav-link:focus-within {
-			background-color: var(--color-nav-hover);
-		}
-		.subnav {
-			position: relative;
-			cursor: pointer;
-			width: 100%;
-		}
-		.subnav .subnav-btn {
-			display: flex;
-			place-items: center;
-			justify-content: end;
-			border: none;
-			outline: none;
-			background-color: transparent;
-			font-family: inherit;
-			cursor: pointer;
-			margin-left: -0.125rem;
-		}
-		.subnav:focus-within {
-			background-color: var(--color-nav-hover);
-		}
-		.subnav:focus-within .subnav-content {
-			display: flex;
-		}
-		.subnav-content {
-			display: none;
-			flex-direction: column;
-			gap: 0.375rem;
-			position: absolute;
-			top: -0.813rem;
-			right: 8.625rem;
-			background-color: var(--color-brand-1);
-			z-index: 1;
-			border-radius: 0.375rem;
-			text-align: right;
-			min-width: 11rem;
-			padding: 0.75rem 0;
-		}
-		.subnav-link {
-			padding: 0.25rem 0.625rem;
-		}
-		.register-link {
-			font-family: 'Brasspounder';
-			background-color: var(--color-brand-1-c);
-			border: 0 solid;
-			border-radius: 0.375rem;
-			color: var(--color-brand-1);
-			text-decoration: none;
-			padding: 0.625rem 1.25rem;
-			margin: 0.375rem 0.625rem;
-			display: inline-block;
-		}
-	`;
-	const desktopStylesheet = desktopCSS`
-		.nav {
-			display: flex;
-			gap: 0.5rem;
-			height: 3rem;
-		}
-		.nav-link {
-			color: var(--color-brand-1-c);
-			display: flex;
-			place-items: center;
-			padding: 0 1rem;
-			height: 100%;
-			text-decoration: none;
-			font-size: 1.2rem;
-		}
-		.nav-link:hover {
-			background-color: var(--color-nav-hover);
-		}
-		.nav > .nav-link {
-			height: 100%;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			padding: 0 0.75rem;
-		}
-		.subnav {
-			display: flex;
-			position: relative;
-			height: 100%;
-			cursor: pointer;
-		}
-		.subnav .subnav-btn {
-			display: flex;
-			place-items: center;
-			border: none;
-			outline: none;
-			background-color: transparent;
-			font-family: inherit;
-			cursor: pointer;
-		}
-		.subnav:hover {
-			background-color: var(--color-nav-hover);
-		}
-		.subnav:hover .subnav-content {
-			display: flex;
-		}
-		.subnav-content {
-			display: none;
-			flex-direction: column;
-			gap: 0.375rem;
-			position: absolute;
-			top: 3rem;
-			right: 0;
-			background-color: var(--color-brand-1);
-			z-index: 1;
-			border-radius: 0.375rem;
-			text-align: right;
-			min-width: 15rem;
-			padding: 0.75rem 0;
-		}
-		.subnav-link {
-			padding: 0.25rem 0.625rem;
-		}
-		.register-link {
-			font-family: 'Brasspounder';
-			background-color: var(--color-brand-1-c);
-			border: 0 solid;
-			border-radius: 0.5rem;
-			color: var(--color-brand-1);
-			text-decoration: none;
-			padding: 1rem 2rem;
-			display: inline-block;
-		}
-	`;
 	const stylesheet = mobile ? mobileStylesheet : desktopStylesheet;
 	const global = useContext(GlobalContext);
 	const nav = getTypedContent<LinkContent>(global.nav);
@@ -178,28 +177,45 @@ const PageNav = ({ isOpen = true, mobile = false, ...forwardedProps }: PageNavPr
 	return (
 		<Scope {...forwardedProps} tag={PAGE_NAV_TAG} stylesheets={[theme, stylesheet]}>
 			<div className="wrapper">
-				<nav className="nav">
-					{nav.map(({ content, childContent: children }) => {
-						if (Array.isArray(children) && children.length > 0) {
-							return (
-								<div key={content.href} className="subnav">
-									<button className="nav-link subnav-btn">
-										{mobile ? <Icon type="left-chevron" /> : <></>}
-										<span>{content.text}</span>
-										{mobile ? <></> : <Icon type="down-chevron" />}
-									</button>
-									<nav className="subnav-content">
-										{getTypedContent<LinkContent>(children).map(({ content }) => (
-											<Link key={content.href} href={content.href} className="nav-link subnav-link">
-												{content.text}
-											</Link>
-										))}
-									</nav>
-								</div>
-							);
-						}
-
-						return (
+				<nav className="nav" aria-hidden={!isOpen}>
+					{nav.map(({ id, content, childContent }) => {
+						// eslint doesn't recognize this as a component, even though it is
+						// eslint-disable-next-line
+						const [subnavOpen, setSubnavOpen] = useState(false);
+						const toggleSubnav = () => {
+							if (!mobile) return;
+							setSubnavOpen(!subnavOpen);
+						};
+						const handleHover = (on: boolean) => () => {
+							if (mobile) return;
+							setSubnavOpen(on);
+						};
+						return Array.isArray(childContent) && childContent.length > 0 ? (
+							<div
+								key={content.href}
+								className="subnav"
+								onMouseOver={handleHover(true)}
+								onMouseLeave={handleHover(false)}
+							>
+								<button
+									className="nav-link subnav-btn"
+									onClick={toggleSubnav}
+									aria-controls={id}
+									aria-expanded={subnavOpen}
+								>
+									{mobile ? <Icon type="left-chevron" /> : <></>}
+									<span>{content.text}</span>
+									{mobile ? <></> : <Icon type="down-chevron" />}
+								</button>
+								<nav className="subnav-content" id={id} aria-hidden={!subnavOpen}>
+									{getTypedContent<LinkContent>(childContent).map(({ content }) => (
+										<Link key={content.href} href={content.href} className="nav-link subnav-link">
+											{content.text}
+										</Link>
+									))}
+								</nav>
+							</div>
+						) : (
 							<Link key={content.href} href={content.href} className="nav-link">
 								{content.text}
 							</Link>
