7779d4f7899ef852b5b39e32c5dcbf864a5828bb
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2023-11-18 13:44:19 -0500

    removing quicksand, relying instead on next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,6 @@ const RootLayout = ({ children }: PropsWithChildren) => {
 		<html lang="en">
 			<head>
 				<link rel="icon" href="/favicon.ico" />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
-					rel="stylesheet"
-				/>
 			</head>
 			<body className={font.className}>
 				<GlobalProvider value={global}>
