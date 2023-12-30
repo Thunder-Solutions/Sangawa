c846bf173f0278cba9cfee659cc4fbf92db64c7e
rtmoffat <rtmoffat@gmail.com>
2023-12-30 17:28:02 -0500

    Added events to events page

--- src/api-client/methods/events-page.ts
+++ src/api-client/methods/events-page.ts
@@ -27,10 +27,51 @@ export const fetchEventsPageContent = async (): Promise<Result<ContentSet>> => {
 					},
 					childContent: [
 						{
-							id: 'e5b7ba0a-202b-4b1f-b725-956f20c9f28e',
+							id: '0d935233-ff0f-4244-b2ac-3ccf7c828b49',
 							content: {
 								component: 'Text',
-								prop__text: 'Events to be announced soon, stay tuned!',
+								prop__text: `![Taste of Japan Event](/events/taste.png "Taste of Japan")
+
+Attention all anime enthusiasts aged **21 and above**!
+
+The exclusive Taste of Japan room is making its return at Sangawa Project!
+
+This event is available only to **Jōnin & Kage** badge Upgrades. 
+
+Dive into the Taste of Japan experience:
+
+* Indulge in a selection of Japanese sake, whiskey, soju, and more
+
+* Learn information on each drink while you enjoy it!
+
+* Saturday muli hour access lets you enjoy without missing any convention content.
+
+**Limited to 100 lucky attendees**, so grab your tickets swiftly!
+Hurry, secure your spot for this exclusive tasting extravaganza! Don't miss out on this unique opportunity at Sangawa Project!
+Stay tuned for updates on this year's beverage selections! Let's elevate your anime experience with a Taste of Japan!`,
+							},
+						},
+						{
+							id: '5ab81d46-4300-4374-848c-99cf43bebfd2',
+							content: {
+								component: 'Text',
+								prop__text: `![The Sangawa Social](/events/social_tldr.png "The Sangawa Social")
+
+Kick off your Sangawa weekend in style at **The Sangawa Social!**
+
+Join us on **Friday Feb 9th** for a night of:
+								
+* Mingling with old and new friends
+								
+* Themed libations that set the tone for an epic weekend
+								
+* Try Mystic Mixtures' famed Health Potion cocktail!
+								
+* Exclusive to Chūnin & Kage upgrade holders
+								
+* Showcase your 2024 Sangawa Tumbler in style
+								
+This is your chance to start the fun early! Don't miss out!`,
 							},
 						},
 					],
