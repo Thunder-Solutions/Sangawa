e76e62cb61bc64987aa2b0368449c2266d701e8d
Jon DeWitt <jon.dewitt@thunder.solutions>
2024-09-04 19:55:52 -0400

    fixing build

--- src/api-client/context.tsx
+++ src/api-client/context.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { ReactNode, createContext } from 'react';
-import { ContentSet } from './api-client';
+import { ContentSet, DEFAULT_CONTENT_SET } from './api-client';
 
-export const GlobalContext = createContext<ContentSet>({});
+export const GlobalContext = createContext<ContentSet>(DEFAULT_CONTENT_SET);
 
 export const GlobalProvider = ({ value, children }: { value: any; children: ReactNode }) => {
 	return <GlobalContext.Provider value={value}>{children}</GlobalContext.Provider>;
